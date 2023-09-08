e4b77364fa16ab0e975b2e6b5d255dd31c64c63b
psk-98 <paulk198@hotmail.com>
2023-09-08 13:34:53 +0200

    time format

--- stage_one_app/views.py
+++ stage_one_app/views.py
@@ -9,11 +9,12 @@ def index(request):
 
     if request.method == 'GET':
         print(request.method)
-   
+        
+        now = datetime.datetime.now()
         return JsonResponse({
             "slack_name" : request.GET.get('slack_name'),
             "current_day": daysOfTheWeek[datetime.datetime.today().weekday()],
-            "utc_time": datetime.datetime.now(),
+            "utc_time": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
             'track' :request.GET.get('track'),
             'github_file_url' : "https://github.com/psk-98/stage_one/blob/trunk/stage_one_app/views.py",
             'github_repo_url' : "https://github.com/psk-98/stage_one",
