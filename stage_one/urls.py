0ec2e08ce01c57b7f074d13480d45bc52785a0df
psk-98 <paulk198@hotmail.com>
2023-09-08 12:58:30 +0200

    change route to api

--- stage_one/urls.py
+++ stage_one/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stageone/', include('stage_one_app.urls')),
+    path('api/', include('stage_one_app.urls')),
 ]
